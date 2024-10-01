d92655fccbe9981d8db359ce89afa30eea0e6464
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-01 15:19:47 +0530

    Document header added

---
+++ student_details1.py
@@ -0,0 +1,14 @@
+#Author: Sreya
+Date:01-10-2024
+Python program to get the student details
+Version:1.0
+
+name=input("Enter the student name:")
+roll_number=int(input("Enter your roll number:"))
+print("Name of the student",name)
+print("Roll number of the student",roll_number)
+cgp=float(input("Enter cgp of the student:"))
+print("cgp of the student is:",cgp)
+percentage=("Enter percentage of the student:")
+percentage=cgp*10
+print("Percentage of the student is:",percentage+"%")
