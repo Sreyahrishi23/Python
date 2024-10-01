617cca3cca1b47437e7e948800355e4f0ff4cae5
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-01 14:51:12 +0530

    My first commit

---
+++ student_details.py
@@ -0,0 +1,9 @@
+name=input("Enter the student name:")
+roll_number=int(input("Enter your roll number:"))
+print("Name of the student",name)
+print("Roll number of the student",roll_number)
+cgp=float(input("Enter cgp of the student:"))
+print("cgp of the student is:",cgp)
+percentage=("Enter percentage of the student:")
+percentage=cgp*10
+print("Percentage of the student is:",percentage+"%")
