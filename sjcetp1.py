089bf0a0ad3c1cf5d293342e575703b6051bed2f
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-07 23:16:08 +0530

    Sjcetp1.py

--- sjcetp1.py
+++ sjcetp1.py
@@ -1,3 +1,9 @@
+'''
+Author:Sreya
+Date:07-10-2024
+Write a python program that asks the user to input their name and age. After receiving the input,display a messgae that includes the user's name and age.
+Version:3.11.4
+'''
 name=input("Enter your name:")
 age=int(input("Enter your age:"))
 print("Hello",name,"!You are",age,"years old")
