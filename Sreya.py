eacf2301bfabcf5546fd7bc6717392754772f9a5
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-08 14:21:34 +0530

    Simple desktop calculator using Python.

---
+++ Sreya.py
@@ -0,0 +1,22 @@
+'''
+Author: Sreya
+Date: 08-10-2024
+Version: 1.0
+Simple desktop calculator using Python.
+'''
+
+num1=int(input("Enter a number"))
+num2=int(input("Enter a number"))
+num3=int(input("Enter a number"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+difference=num1-num2
+print("The difference when num2 is subtracted from num1 is:",difference)
+product=num1*num2
+print("The product of num1 and num2 is:",product)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is:",division)
+modulus=num1%num2
+print("The remainder when num1 is divided by num2 is:",modulus)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is:",result)
