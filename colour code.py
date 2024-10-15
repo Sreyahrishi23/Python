f95a84e84b07d35cd791c8bfc87087d0c50d9eab
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 15:12:34 +0530

    Colour based on code value

---
+++ colour code.py
@@ -0,0 +1,15 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to print the colour based on a code value
+Version;3.11.4
+'''
+Grade=input("Enter grade:")
+if Grade=="R":
+    print("Message=Red")
+elif Grade=="G":
+    print("Message=Green")
+elif Grade=="B":
+    print("Message=Blue")
+else:
+    print("Wrong code")
