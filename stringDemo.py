1ae1118ca7fef04c3568bb4c54a6b6669accc173
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 14:05:50 +0530

    stringDemo.py
    Python program to check whether the given number is positive or not

---
+++ stringDemo.py
@@ -0,0 +1,11 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to check whether the given number is positive or not
+Version;3.11.4
+'''
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+else:
+    print("The given number is negative")
