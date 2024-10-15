d5a65f851cdb9ec56d0ab823ac8153221af3c1bb
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 14:24:48 +0530

    M2 E2

---
+++ ex 2.py
@@ -0,0 +1,11 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to check if a person is eligible for a driving license
+Version;3.11.4
+'''
+age=int(input("Enter your age:"))
+if age>=16:
+    print("Eligible for a driving license")
+else:
+    print("Not eligible for a driving license")
