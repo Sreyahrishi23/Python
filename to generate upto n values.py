6667ff16df238501abb41949b2e0ff3d8010487f
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 16:07:10 +0530

    to generate upto n values

---
+++ to generate upto n values.py
@@ -0,0 +1,11 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to generate upto n values
+Version;3.11.4
+'''
+limit=int(input("Enter the limit:"))
+odd_number=1
+while odd_number<=limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
