d9aacf1a2c04c4fc9492b024cfe7d76193f67921
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 15:35:58 +0530

    div by 5

---
+++ divisible by 5.py
@@ -0,0 +1,11 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to check whether the given number is divisible by 5
+Version;3.11.4
+'''
+num=int(input("Enter a number:"))
+if num%5==0:
+    print("Divisible by 5")
+else:
+    print("Not divisble by 5")
