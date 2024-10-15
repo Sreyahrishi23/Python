7d83d08e19d22cd294bfe848425894d62eb345f2
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 14:35:22 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,11 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to find simple interest
+Version;3.11.4
+'''
+principle=int(input("Enter principle amount:"))
+rate=int(input("Enter the amount:"))
+year=int(input("Enter year:"))
+simple_interest=principle*rate*year
+print(simple_interest)
