34569b05541d5b03bf7713557c60bd441e9052a0
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 14:20:28 +0530

    M2 E1

---
+++ ex 1.py
@@ -0,0 +1,9 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to check if a number is greater than 100
+Version;3.11.4
+'''
+number=int(input("Enter a number:"))
+if number>100:
+    print("The given number is greater than 100")
