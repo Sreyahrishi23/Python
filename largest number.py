a686b9ec17913d26e80ff6756ca616fc4d76621e
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-15 14:42:28 +0530

    largest number

---
+++ largest number.py
@@ -0,0 +1,14 @@
+'''
+Date:15-10-2024
+Author:Sreya Hrishikesh
+Python program to determine the largest of two numbers
+Version;3.11.4
+'''
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+if num1>num2:
+    print("num1 is greater than num2")
+elif num1<num2:
+    print("num2 is greater than num1")
+else:
+    print("num1 and num2 are equal")
