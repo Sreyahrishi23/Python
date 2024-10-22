d34420fdea9211cf51204e5f10d42cab656ef0ce
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-22 15:23:30 +0530

    Task 4

---
+++ Task 4.py
@@ -0,0 +1,9 @@
+'''
+Author:Sreya
+Date:07-10-2024
+Write a Python program that uses function from the math module to perform the following operations on a number provided by the user
+Version:3.11.4
+'''
+import math
+number=int(input("Enter number:"))
+print("")
