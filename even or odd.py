c6f116a8fb835c418655489aa1aae915e1f4462e
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2024-10-29 14:41:17 +0530

    to check if the given number is even or odd

---
+++ even or odd.py
@@ -0,0 +1,12 @@
+'''
+Author: Sreya
+Date: 29-10-2024
+Version: 1.0
+Python program to check if the given number is odd or even
+'''
+
+num=int(input("Enter a positive number:"))
+if num%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
